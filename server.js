c63008b8770b5c76bee0735aa36f679f3b4a8601
Ananda Pamungkas <gedeananda@hotmail.com>
2020-09-20 14:49:21 +0800

    a simple nodemailer app

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config(); //require dotenv package
+const nodemailer = require('nodemailer'); //require nodemailer package
+
+//step 1 make a transporter
+let transporter = nodemailer.createTransport({
+    //transporter connect ot mail hosting
+    service: 'gmail', //if you're using gmail please make sure yu activated less secure app in google console
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+//step 2
+
+let mailOptions = {
+    //email data
+    from: 'emailsender@email.com',
+    to: 'emailreceiver1@email.com, emailreceiver2@email.com',
+    subject: 'Testing and testing',
+    text: 'IT works'
+}
+
+//step 3
+transporter.sendMail(mailOptions, function(err, data) {
+    //send the email
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Email sent!');
+    }
+});
